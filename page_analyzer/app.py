3ba767c7f5ba64b7b77168753463c6ee7c066a37
Egor Gerimovich <cherund@gmail.com>
2024-04-29 11:44:29 +0200

    add /urls route to app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,4 +6,9 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Hello world!'
+    return flask.render_template('index.html')
+
+
+@app.route('/urls')
+def get_urls():
+    return flask.render_template('urls.html')
