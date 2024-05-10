46c1f58fa1feb536ef303891ab331f278704f100
Egor Gerimovich <cherund@gmail.com>
2024-05-10 11:10:12 +0200

    update logic and add variables to pages in app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 import flask
-from page_analyzer.utils import add_item, get_item
+from page_analyzer.utils import (add_item, get_item,
+                                 check_url, normalize_url,
+                                 get_all, get_env_var)
 
 
 app = flask.Flask(__name__)
+app.secret_key = get_env_var('SECRET_KEY')
 
 
 @app.route('/')
 def main():
-    return flask.render_template('index.html')
+    messages = flask.get_flashed_messages(with_categories=True)
+    return flask.render_template('index.html', messages=messages,)
 
 
 @app.route('/urls')
 def get_urls():
-    return flask.render_template('urls.html')
+    urls = get_all()
+    return flask.render_template('urls.html', urls=urls)
 
 
 @app.route('/url/<int:url_id>')
@@ -26,5 +31,11 @@ def show_url_page(url_id):
 @app.post('/url')
 def add_url():
     url = flask.request.form.get('url')
+    message = check_url(url)
+    if message:
+        flask.flash(message, 'error')
+        return flask.redirect(flask.url_for('main'))
+
+    url = normalize_url(url)
     url_id = add_item(url)
     return flask.redirect(flask.url_for('show_url_page', url_id=url_id))
