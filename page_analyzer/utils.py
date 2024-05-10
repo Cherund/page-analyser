b01182f48506fa1b7e166bc7c97e186088b3c91d
Egor Gerimovich <cherund@gmail.com>
2024-05-10 11:08:57 +0200

    add logic and functions to utils.py

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,11 +1,13 @@
+import os
+import validators
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
-
-
-DATABASE_URL = 'postgresql://pguser:pgpass@localhost:5432/pgdb'
+from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 
 def add_item(url):
+    DATABASE_URL = get_env_var('DATABASE_URL')
     conn = psycopg2.connect(DATABASE_URL)
     with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
@@ -18,9 +20,38 @@ def add_item(url):
 
 
 def get_item(url_id):
+    DATABASE_URL = get_env_var('DATABASE_URL')
     conn = psycopg2.connect(DATABASE_URL)
     with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
             'SELECT * FROM urls WHERE id=(%s);', (url_id, )
         )
         return curs.fetchone()
+
+
+def check_url(url):
+    if len(url) > 255:
+        return 'URL exceeds 255 symbols'
+
+    if not validators.url(url):
+        return 'Incorrect URL'
+
+
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    return f'{parsed_url.scheme}://{parsed_url.netloc}'
+
+
+def get_env_var(var_name):
+    load_dotenv()
+    return os.environ.get(var_name)
+
+
+def get_all():
+    DATABASE_URL = get_env_var('DATABASE_URL')
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute(
+            'SELECT * FROM urls',
+        )
+        return list(curs.fetchall()[::-1])
