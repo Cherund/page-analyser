5bf1825c8407dceb8211de76abff7ebd2e998248
Egor Gerimovich <cherund@gmail.com>
2024-05-21 10:45:20 +0200

    leave only bs4 part on bs_util.py

--- page_analyzer/bs_util.py
+++ page_analyzer/bs_util.py
@@ -1,40 +1,18 @@
 from bs4 import BeautifulSoup
-import requests
 
 
-def get_url_response(url):
-    response = requests.get(url)
+def get_url_info(url_response):
 
-    if requests.Response.raise_for_status(response):
-        raise requests.RequestException
+    soup = BeautifulSoup(url_response.text, 'html.parser')
 
-    return response
+    h1_tag = soup.find('h1')
+    title_tag = soup.find('title')
+    description_tag = soup.find('meta', attrs={'name': 'description'})
 
-
-def get_tag_str(url_content, tag, attrs={}):
-    soup = BeautifulSoup(url_content, 'html.parser')
-    tag_str = soup.find(name=tag, attrs=attrs)
-    if tag_str:
-        text = tag_str.text or tag_str['content']
-        if len(text) > 255:
-            # text_list = text.split()
-            # text_list[-1] = '...'
-            # return ' '.join(text_list)
-            return text[:252] + '...'
-        return text
-    else:
-        return ''
-
-
-def get_url_info(url):
-    try:
-        url_response = get_url_response(url)
-    except requests.RequestException:
-        return None
-
-    h1 = get_tag_str(url_response.content, 'h1')
-    title = get_tag_str(url_response.content, 'title')
-    description = get_tag_str(url_response.content, 'meta',
-                              {'name': "description"})
-
-    return url_response.status_code, h1, title, description
+    return {
+        'h1': h1_tag.text[:255] if h1_tag else '',
+        'title': title_tag.text[:255] if title_tag else '',
+        'status_code': url_response.status_code,
+        'description': (description_tag.get('content', '')[:255]
+                        if description_tag else '')
+    }
